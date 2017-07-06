6787378d6a19792e58e1685142e159362cf614aa
Jordan Zhang <chrn.zhng@gmail.com>
2017-07-06 10:43:13 -0600

    Step 7

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var massive = require('massive');
+
+var db = massive.connectSync({
+  connectionString : 'postgres://yclvuvrelxsxij:78a86282e644ee9c28f285446c54e67dffd00359ec19eb99db932ec7c202d38f@ec2-23-21-246-11.compute-1.amazonaws.com:5432/d4hc21515iuphb?ssl=true'
+});
 
 var app = express();
 app.use(bodyParser.json());
 
 var port = 3000;
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.get('/', function (req, res) {
+  db.getAllInjuries(function (err, injuries) {
+    res.send(injuries);
+  });
 });
 
 app.get('/incidents', function(req, res) {
