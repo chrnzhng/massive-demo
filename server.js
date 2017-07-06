8587b999976d9a8a6dd168c6ee5bce0b4a2bcfcf
Jordan Zhang <chrn.zhng@gmail.com>
2017-07-06 12:31:05 -0600

    Trying to figure out lodash

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var massive = require('massive');
-
+var _ = require('lodash');
 var db = massive.connectSync({
   connectionString : 'postgres://yclvuvrelxsxij:78a86282e644ee9c28f285446c54e67dffd00359ec19eb99db932ec7c202d38f@ec2-23-21-246-11.compute-1.amazonaws.com:5432/d4hc21515iuphb?ssl=true'
 });
@@ -12,9 +12,26 @@ app.use(bodyParser.json());
 var port = 3000;
 
 app.get('/', function (req, res) {
-  db.getAllInjuries(function (err, injuries) {
-    res.send(injuries);
-  });
+  var cars = [
+    {plate: 'XYZ'},
+    {plate: 'ABC'},
+  ];
+
+  var cake = _.template(`
+    <h1>hello <%= name %></h1>
+    <% cars.forEach(function(inury) { %>
+      <div><%= injury.name %> - <% injury.description %></div>
+  <% }) %>
+  `);
+  })
+  db.getAllInjuries(function(err, injuries){
+  var html = cake({
+    // name: 'Jordan',
+    // cars: cars,
+    injuries: injuries
+    
+  })
+  res.send(html)
 });
 
 app.get('/incidents', function(req, res) {
